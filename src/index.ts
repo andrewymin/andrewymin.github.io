a26058d7279dad4a8e30c1d28ffa6eb741766f63
Andy <yoongi9189@gmail.com>
2023-01-12 21:27:51 -0700

    Add typescript and project nav

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+let d:Date = new Date();
+let year:Number = d.getFullYear();
+$(".year").text(String(year));
+
+let projectNavItems = document.querySelectorAll('.pn-item');
+let projects = document.getElementById('portfolio')?.querySelectorAll('div');
+let pythonProjects = document.querySelectorAll('.python');
+let webDevProjects = document.querySelectorAll('.webDev');
+
+console.log(projects)
+
+projectNavItems.forEach(link => {
+    link.addEventListener("click", e => {
+        projectNavItems.forEach(link => {
+            link.classList.remove('proj-active');
+        });
+        e.preventDefault();
+        link.classList.add('proj-active'); 
+
+        if ($("ul li:nth-child(1)" ).hasClass("proj-active")) {
+            // make the class with portfolio > python disapear; vice versa with portfolio > web for ul li:nth-child(3)
+            projects?.forEach(p => {
+                p.classList.add('appear')
+                p.classList.remove('disappear')
+            })
+            console.log('true')
+        }
+        if ($("ul li:nth-child(2)" ).hasClass("proj-active")) {
+            // make the class with portfolio > python disapear; 
+            pythonProjects.forEach(p => {
+                p.classList.add('disappear')
+                p.classList.remove('appear')
+            })
+            // console.log('true')
+        }
+        if ($("ul li:nth-child(3)" ).hasClass("proj-active")) {
+            // make the class with portfolio > python disapear; vice versa with portfolio > web for ul li:nth-child(3)
+            webDevProjects.forEach(p => {
+                p.classList.add('disappear')
+                p.classList.remove('appear')
+            })
+            console.log('true')
+        }
+        
+      });
+    });
--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -1,3 +1,39 @@
-d = new Date();
-year = d.getFullYear();
-$(".year").text(year);
+"use strict";
+var _a;
+let d = new Date();
+let year = d.getFullYear();
+$(".year").text(String(year));
+let projectNavItems = document.querySelectorAll('.pn-item');
+let projects = (_a = document.getElementById('portfolio')) === null || _a === void 0 ? void 0 : _a.querySelectorAll('div');
+let pythonProjects = document.querySelectorAll('.python');
+let webDevProjects = document.querySelectorAll('.webDev');
+console.log(projects);
+projectNavItems.forEach(link => {
+    link.addEventListener("click", e => {
+        projectNavItems.forEach(link => {
+            link.classList.remove('proj-active');
+        });
+        e.preventDefault();
+        link.classList.add('proj-active');
+        if ($("ul li:nth-child(1)").hasClass("proj-active")) {
+            projects === null || projects === void 0 ? void 0 : projects.forEach(p => {
+                p.classList.add('appear');
+                p.classList.remove('disappear');
+            });
+            console.log('true');
+        }
+        if ($("ul li:nth-child(2)").hasClass("proj-active")) {
+            pythonProjects.forEach(p => {
+                p.classList.add('disappear');
+                p.classList.remove('appear');
+            });
+        }
+        if ($("ul li:nth-child(3)").hasClass("proj-active")) {
+            webDevProjects.forEach(p => {
+                p.classList.add('disappear');
+                p.classList.remove('appear');
+            });
+            console.log('true');
+        }
+    });
+});
